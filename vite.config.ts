453092b6ba93bad45af039d3cc63eeaeca8020fc
Ilham Nofaldi <2211522028_ilham@student.unand.ac.id>
2025-05-21 10:41:08 +0700

    first commiiiit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 8080,
+    host: true, // This tells Vite to listen on all available network interfaces (0.0.0.0)
+    strictPort: true, // Vite will exit if the port is already in use
+    allowedHosts: ['my-app-28630741999.asia-southeast2.run.app']
+  }
 })
